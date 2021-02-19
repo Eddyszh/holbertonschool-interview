df7f8cc6fc62d02e2749fa1d4de4a4b060d26542
eddyszh <eddyszh@gmail.com>
2021-02-18 20:11:06 -0500

    Fix pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,22 @@ utf-8 validation module
 """
 
 
-def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
-    count = 0
-    for c in data:
-        if count == 0:
-            if (c >> 5) == 0b110:
-                count = 1
-            elif (c >> 4) == 0b1110:
-                count = 2
-            elif (c >> 3) == 0b11110:
-                count = 3
-            elif (c >> 7):
-                return False
-        else:
-            if (c >> 6) != 0b10:
-                return False
-            count -= 1
-    return count == 0
+if __name__ == "__main__":
+    def validUTF8(data):
+        """determines if a given data set represents a valid UTF-8 encoding"""
+        count = 0
+        for c in data:
+            if count == 0:
+                if (c >> 5) == 0b110:
+                    count = 1
+                elif (c >> 4) == 0b1110:
+                    count = 2
+                elif (c >> 3) == 0b11110:
+                    count = 3
+                elif (c >> 7):
+                    return False
+            else:
+                if (c >> 6) != 0b10:
+                    return False
+                count -= 1
+        return count == 0
