6c4d572374f19a798dbd2d5584a2d66c53cc3c02
eddyszh <eddyszh@gmail.com>
2021-04-15 18:09:49 -0500

    Add python rain project

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rain algorithm
+"""
+
+
+def rain(walls):
+    """calculate how much water will be retained after it rains
+    """
+    left = 0
+    rigth = len(walls) - 1
+    level = 0
+    water = 0
+    while (left < rigth):
+        if walls[left] < walls[rigth]:
+            low = walls[left]
+            left +=1
+        else:
+            low = walls[rigth]
+            rigth -= 1
+        if (level < low):
+            level = low
+        water += level - low
+    return water
